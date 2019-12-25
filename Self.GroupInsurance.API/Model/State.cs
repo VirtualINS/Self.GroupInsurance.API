a7161a7e63d1d8d2498500393bddfe8a5f43bb0d
Aniruddha Chakraborty <chakanc@MFCGD.COM>
2019-12-25 15:36:22 -0500

    State Introduction.

---
+++ Self.GroupInsurance.API/Model/State.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Self.GroupInsurance.API.Model
+{
+    public class State : TableEntity
+    {
+        public State()
+        {
+            //this.PartitionKey = Country;
+            this.RowKey = Guid.NewGuid().ToString();
+            //this.Timestamp = DateTimeOffset.Now;
+            //this.ETag = Country;
+        }
+
+        public string ID { get; set; }
+
+        public string StateName { get; set; }
+
+        public string Country { get; set; }
+    }
+
+}
